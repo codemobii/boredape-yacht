256729b6d33d9ca9fdba599f3b4f83c582ce2746
John <45017704+codemobii@users.noreply.github.com>
2022-03-30 00:02:40 +0100

    Update mint.js

--- mint.js
+++ mint.js
@@ -51,6 +51,8 @@ const initialize = () => {
       // Will open the MetaMask UI
       // You should disable this button while the request is pending!
       await ethereum.request({ method: "eth_requestAccounts" });
+
+      window.location.reload();
     } catch (error) {
       console.error(error);
     }
